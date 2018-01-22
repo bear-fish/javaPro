3195f2c26cfc65d253b0bbd7e2fbafcd50682db0
bear-fish <35320676+bear-fish@users.noreply.github.com>
2018-01-22 14:41:13 +0800

    Update and rename p5q2.asm to c5q2.asm
    更名为c5p2,修改代码格式

---
+++ c5q2.asm
@@ -0,0 +1,14 @@
+; 编程, 向内存0:200-0:23F依次传送数据0-63(3FH).
+assume cs:code
+code segment
+start: mov ax,20h
+       mov ds,ax
+       mov bx,0; ds:[bx]指向内存0:200h处
+       mov cx,63; 循环次数63次
+s:     mov [bx],bx
+       inc bx
+       loop s
+       mov ax,4c00h
+       int 21h
+code ends
+end start
